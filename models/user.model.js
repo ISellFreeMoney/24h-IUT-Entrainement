295f8b18ad2f0c954dd6005921b7d2349dcb4afd
ISellFreeMoney <minegamerfr@gmail.com>
2021-04-04 17:27:50 +0200

    Added user control (get info, update, delete)

--- models/user.model.js
+++ models/user.model.js
@@ -1,60 +1,55 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const { isEmail } = require('validator');
-const userSchema = new mongoose.Schema(
-    {
-        pseudo: {
-            type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 55,
-            unique: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            require: true,
-            validate: [isEmail],
-            lowercase: true,
-            unique: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            require: true,
-            maxLength: 1024,
-            minLength: 6,
-            trim: true
-        },
-        picture: {
-            type: [String],
-            default: "./upload/profil/random-user.png"
-        },
-        bio: {
-            type: String,
-            max: 1024,
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { isEmail } = require('validator')
+const userSchema = new mongoose.Schema({
+  pseudo: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 55,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    require: true,
+    validate: [isEmail],
+    lowercase: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    require: true,
+    maxLength: 1024,
+    minLength: 6,
+    trim: true
+  },
+  picture: {
+    type: [String],
+    default: "./upload/profil/random-user.png"
+  },
+  bio: {
+    type: String,
+    max: 1024
+  },
+  followers: {
+    type: [String]
+  },
+  following: {
+    type: [String]
+  },
+  likes: {
+    type: [String]
+  }
+}, {
+  timestamps: true
+})
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
 
-        },
-        followers: {
-            type: [String]
-        },
-        following: {
-            type: [String]
-        },
-        likes: {
-            type: [String]
-        }
-    },
-    {
-        timestamps: true
-    }
-    
-)
-userSchema.pre("save", async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-const UserModel = mongoose.model('user', userSchema);
-module.exports = UserModel;
+const UserModel = mongoose.model('user', userSchema)
+module.exports = UserModel
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller.js');
+const userController = require('../controllers/user.controller');
 
+
+// auth 
 router.post('/register', authController.signUp);
+
+
+// user DB
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+router.put("/:id", userController.updateUsers);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
